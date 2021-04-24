002703e57bc9d5425352d2d2f5100d9c05e31ef7
Marco Consiglio <m.consiglio@sermetra.it>
2021-04-24 10:08:02 +0200

    Il database funziona!

--- app/public/db_test.php
+++ app/public/db_test.php
@@ -1,8 +1,54 @@
 <?php
-$pdo = new PDO('mysql:host=mysql;dbname=tutorial', 'tutorial', 'secret', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+$pdo = new PDO('mysql:host=mysql;dbname=database', 'developer', 'secret', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 
 $query = $pdo->query('SHOW VARIABLES like "version"');
 
-$row = $query->fetch();
+$version = $query->fetch();
 
-echo 'MySQL version:' . $row['Value'];
+$query = $pdo->query("SHOW DATABASES;");
+
+$databases = $query->fetchAll();
+
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+
+    <title>DB Test</title>
+  </head>
+  <body>
+    
+    <div class="container mt-3">
+        <div class="row">
+            <div class="col-md-3"></div>
+            <div class="col">
+                <h1 class="display-4"><?= "MySQL version: ".$version['Value'] ?></h1>
+                <h2>Databases</h2>
+                <ul>
+                    <?php foreach ($databases as $database): ?>
+                        <li><?=$database[0]?></li>
+                    <?php endforeach; ?>
+                </ul>
+            </div>
+            <div class="col-md-3"></div>
+        </div>
+    </div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
